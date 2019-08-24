18f9db0bebf3682e31de65ccff68681275664454
Matthew Lockdall <mlockdall26@gmail.com>
2019-08-24 01:18:07 -0500

    intersection observer on titles

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 console.log(1 + 1);
 const cards = document.querySelectorAll('.anim')
+const title = document.querySelectorAll('.tit')
 const options = {
   root: null,
   rootMargin: '0px',
   threshold: .8
 }
 
-observer = new IntersectionObserver((entries, options) => {
+const observer = new IntersectionObserver((entries, options) => {
   console.log(entries)
   entries.forEach((rep) => {
     if (rep.intersectionRatio > 0) {
@@ -18,7 +19,20 @@ observer = new IntersectionObserver((entries, options) => {
   })
 
 })
+
+const titobserver = new IntersectionObserver((entries, options) => {
+  entries.forEach(rep => {
+    if (rep.intersectionRatio > 0) {
+      rep.target.style.animation = `anim2 1s ${rep.target.dataset.delay} forwards ease-in`
+    } else {
+      rep.target.style.animation = 'none'
+    }
+  })
+})
+
 cards.forEach(c => {
   observer.observe(c)
 })
-// observer.observe(cards)
+title.forEach(t => {
+  titobserver.observe(t)
+})
